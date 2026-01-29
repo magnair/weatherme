70604146f70db414286351ab371977ad2145067b
Ola Eriksen <Ola.Eriksen@bouvet.no>
2026-01-29 16:38:34 +0100

    added https://magnair.github.io to cors

--- WeatherMe.Api/Program.cs
+++ WeatherMe.Api/Program.cs
@@ -23,7 +23,8 @@ builder.Services.AddCors(options =>
         policy.WithOrigins(
                 "http://localhost:3000",
                 "http://localhost:5173",
-                "https://localhost:5001")
+                "https://localhost:5001",                
+                "https://magnair.github.io")
               .AllowAnyHeader()
               .AllowAnyMethod()
               .AllowCredentials();
